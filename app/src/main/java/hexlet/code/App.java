dfd9d9b9f1f8bd4f4ecf3d3e6c54a37722622e89
arlen asanov <asnvarlen@gmai.com>
2023-12-19 20:07:15 +0600

    make game "Even" and add link to asciinema

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,23 @@
 package hexlet.code;
 
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.sayHello();
+
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet\n" + "2 - Even\n" + "0 - Exit");
+        System.out.print("Your choice: ");
+
+        Scanner scanner = new Scanner(System.in);
+        byte gameNumberSelection = scanner.nextByte();
+        System.out.println();
+
+        switch (gameNumberSelection) {
+            case 1:
+                Cli.sayHello();
+                break;
+            case 2:
+                Even.runGame();
+        }
     }
 }
